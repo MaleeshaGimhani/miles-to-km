cfd2ef260421325a1dbda18aa5d9884f2093c4af
MaleeshaGimhani <147963848+MaleeshaGimhani@users.noreply.github.com>
2023-10-15 10:21:55 +0530

    Update miles-km.py
    Updated the code

--- miles-km.py
+++ miles-km.py
@@ -1,3 +1,15 @@
 # Welcome message
 print("Welcome to the Miles to Kilometers Converter!")
 
+# Input from the user
+miles = float(input("Enter the distance in miles: "))
+
+# Conversion factor
+miles_to_km = 1.60934
+
+# Convert miles to kilometers
+kilometers = miles * miles_to_km
+
+# Display the result
+print(f"{miles} miles is equal to {kilometers:.2f} kilometers.")
+
